20beb09986b5a5003c87ebdc53371eacec5eb9b1
megfly <mflynn333@gmail.com>
2021-01-05 18:48:14 -0500

    created workouts controller with the index and create controller action

---
+++ app/controllers/api/v1/workouts_controller.rb
@@ -0,0 +1,24 @@
+class Api::V1::WorkoutsController < ApplicationController
+
+    def index 
+        #we dont need instance variables because we arent passing the instances to the erb/views
+        workouts = Workout.all
+        render json: workouts
+    end 
+
+    def create 
+        workout = Workout.new(workout_params)
+        if workout.save 
+            render json: workout, status: :accepted
+        else 
+            render json: {errors: workout.errors.full_messages}, status: :unprocessible_entity
+        end 
+    end 
+
+    private 
+#strong params- permiting these attributes to the db, to create a new instance
+    def workout_params 
+        params.require(:workout).permit(:title, :duration, :date)
+    end
+
+end
