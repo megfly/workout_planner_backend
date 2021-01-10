2692f2377e39cf8a0d6298cedeea23220663bd4d
megfly <mflynn333@gmail.com>
2021-01-10 18:50:26 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do 
-      resources :workouts, only: [:index, :create]
+      resources :workouts, only: [:index, :create, :destroy]
+      resources :exercises, only: [:index, :create, :destroy]
 
-      get '/exercises' => 'exercises#index'
-      get '/exercises/:id' => 'exercises#show'
+      # get '/exercises' => 'exercises#index'
+      # get '/exercises/:id' => 'exercises#show'
+      # get '/pizzas' => 'pizzas#index'
+      # get '/pizzas/:id' => 'pizzas#show'
+      # post '/pizzas' => 'pizzas#create'
+      # patch '/pizzas/:id' => 'pizzas#update'
+
+      # post '/toppings' => 'toppings#create'
+      # delete '/toppings/:id', to: 'toppings#destroy' 
       end 
     end 
    
