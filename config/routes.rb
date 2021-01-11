eb15d99750684b6a2a7790b2fad1b7a9ddf7efbd
megfly <mflynn333@gmail.com>
2021-01-11 15:56:36 -0500

    created nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do 
-      resources :workouts, only: [:index, :create, :destroy]
       resources :exercises, only: [:index, :create, :destroy]
+      resources :workouts, only: [:index, :create, :destroy] do 
+        resources :exercises, only: [:create]
 
       # get '/exercises' => 'exercises#index'
       # get '/exercises/:id' => 'exercises#show'
