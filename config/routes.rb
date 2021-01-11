1bb3cc7ab8fef7edc65230b104dc34510f7b071d
megfly <mflynn333@gmail.com>
2021-01-11 16:01:30 -0500

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do 
     namespace :v1 do 
+
       resources :exercises, only: [:index, :create, :destroy]
+
       resources :workouts, only: [:index, :create, :destroy] do 
-        resources :exercises, only: [:create]
+        resources :exercises, only: [:create, :destroy]
+      end 
 
       # get '/exercises' => 'exercises#index'
       # get '/exercises/:id' => 'exercises#show'
