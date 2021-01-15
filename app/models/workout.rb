a47044406102e8f1d8708d6b1620a5ab202aabcf
megfly <mflynn333@gmail.com>
2021-01-15 15:07:53 -0500

    added accepts_nested_attributes

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,3 +1,5 @@
 class Workout < ApplicationRecord
-    has_many :exercises
+    # to ensure that the destroy action on a given workout deletes any associated posts:
+    has_many :exercises, dependent: :destroy
+    accepts_nested_attributes_for :exercises
 end
