24116aaf2ff88a2475581d11c9138c772ac6b81e
megfly <mflynn333@gmail.com>
2021-01-17 10:21:44 -0500

    added a comment in workout.rb, commented out code in exercise_serializer

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ApplicationRecord
-    # to ensure that the destroy action on a given workout deletes any associated posts:
+    # to ensure that the destroy action on a given workout deletes any associated exercises:
     has_many :exercises, dependent: :destroy
     accepts_nested_attributes_for :exercises
 end
--- app/serializers/exercise_serializer.rb
+++ app/serializers/exercise_serializer.rb
@@ -1,5 +1,6 @@
 class ExerciseSerializer
   include JSONAPI::Serializer
-  attributes :name, :reps, :sets, :weight, :workout_id, :workout 
+  attributes :name, :reps, :sets, :weight, :workout_id 
+  # :workout 
   #by adding :workout it allows us to access the associated workout  
 end
